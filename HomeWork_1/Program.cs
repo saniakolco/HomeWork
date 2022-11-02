6ff8c210a5ebcd11fa1cc43466123b41147b8d52
Александр Коваленко <aleksandrkovalenko@iMac-Aleksandr.local>
2022-11-02 21:02:44 +0400

    first commit

---
+++ HomeWork_1/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Input a first number ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a second number ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+int max;
+int min;
+
+if(a > b)
+{
+    Console.WriteLine($"max = {a}");
+    Console.WriteLine($"min = {b}");
+}
+else 
+{
+    Console.WriteLine($"max = {b}");
+    Console.WriteLine($"min = {a}");
+}
